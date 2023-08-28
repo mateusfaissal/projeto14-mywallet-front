5cfb633096e7783f14eff7974f1d523db265c1df
Mateus Faissal <mateusfaissal@gmail.com>
2023-08-28 08:19:06 -0300

    second commit: sign-up page done

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,27 +1,49 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import axios from "axios"
+
 
 export default function SignUpPage() {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const navigate = useNavigate();
+
+  function signUp(e) {
+
+    e.preventDefault();
+
+    if (password !== confPassword) return alert("Password must be equal!");
+
+    axios.post(`${import.meta.env.VITE_API_URL}/sign-up`, { name, email, password })
+    .then(() => navigate("/"))
+    .catch((err) => console.log(err))
+
+  }
+
   return (
-    <SingUpContainer>
-      <form>
+    <SignUpContainer>
+      <form onSubmit={signUp}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
+        <input placeholder="Nome" type="text" value={name} onChange={e => setName(e.target.value)} />
+        <input placeholder="E-mail" type="email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input placeholder="Senha" type="password"  value={password} onChange={e => setPassword(e.target.value)} />
+        <input placeholder="Confirme a senha" type="password" value={confPassword} onChange={e => setConfPassword(e.target.value)} />
         <button>Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to={"/"}>
         Já tem uma conta? Entre agora!
       </Link>
-    </SingUpContainer>
+    </SignUpContainer>
   )
 }
 
-const SingUpContainer = styled.section`
+const SignUpContainer = styled.section`
   height: 100vh;
   display: flex;
   flex-direction: column;
